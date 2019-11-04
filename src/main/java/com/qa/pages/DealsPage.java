3d2aab66006c2f31d1b04aab901ebb72428d3805
sandeepkumargundu <kumar.sandeep528@gmail.com>
2019-11-04 20:35:27 +0530

    Driver Initialization part

--- src/main/java/com/qa/pages/DealsPage.java
+++ src/main/java/com/qa/pages/DealsPage.java
@@ -1,7 +1,15 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class DealsPage 
 {
+	WebDriver driver;
+	public DealsPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
 	public void dealsPage()
 	{
 		System.out.println("Deals Page");
